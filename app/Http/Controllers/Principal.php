4de4650f10133e9fb5f7365e8ab5c395190fadfc
PedrinzxCampos <pedrohenriquejacklaercio@gmail.com>
2026-03-17 14:49:05 -0300

    manipulacao_rotas

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -5,12 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class Principal extends Controller
-{
-    function principal(){
-        echo 'Página Principal';
-    }
 
-    
+{
+
+function principal(){
+
+echo 'Página Principal';
+
 }
 
+function contato(string $nome){
 
+echo $nome;
+
+}
+
+function contatoNomeCompleto(string $nome, string $sobrenome){
+
+echo "O contato é $nome $sobrenome";
+
+}
+
+function contatoMensagem(string $nome, string $sobrenome, string $mensagem){
+
+echo "O contato $nome $sobrenome enviou uma mensagem:";
+
+echo "<h1>$mensagem</h1>";
+
+}
+function contatoTelefone(string $nome, string $sobrenome, string $mensagem, string $telefone, string $email = 'E-mail não Informado'){
+
+    echo "O contato $nome $sobrenome enviou uma mensagem:";
+    echo "<h1>$mensagem</h1>";
+    echo "Telefone: $telefone<br>";
+    echo "E-mail: $email";
+}
+
+}
